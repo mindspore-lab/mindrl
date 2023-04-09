315de93123fda8e2c19d0f4efb71e763d3981081
vector <shiliang10@huawei.com>
2023-04-09 22:18:05 +0800

    fix algo for maddpg and script

--- mindspore_rl/algorithm/maddpg/maddpg_trainer.py
+++ mindspore_rl/algorithm/maddpg/maddpg_trainer.py
@@ -13,23 +13,25 @@
 # limitations under the License.
 # ============================================================================
 """MADDPG Trainer"""
-#pylint: disable=W0613
+
+# pylint: disable=W0613
 import time
-import numpy as np
+
 import mindspore as ms
+import numpy as np
+from mindspore import Parameter, Tensor
 from mindspore.common.api import jit
-from mindspore import Tensor, Parameter
 from mindspore.ops import operations as P
 
-from mindspore_rl.utils.callback import CallbackParam, CallbackManager
 from mindspore_rl.agent.trainer import Trainer
-
+from mindspore_rl.utils.callback import CallbackManager, CallbackParam
 
 
 class MADDPGTrainer(Trainer):
     """This is the trainer class of MADDPG algorithm. It arranges the MADDPG algorithm"""
 
     def __init__(self, msrl, params):
+        # pylint: disable=R1725
         super(MADDPGTrainer, self).__init__(msrl)
         self.msrl = msrl
         self.agent = self.msrl.agent
@@ -45,11 +47,11 @@ class MADDPGTrainer(Trainer):
         self.reduce_mean = P.ReduceMean()
         self.stack_first = P.Stack(axis=0)
         self.cat = P.Concat(axis=1)
-        self.continuous_actions = params.get('continuous_actions')
-        self.num_agent = params.get('num_agent')
-        self.num_eval_episode = params.get('num_eval_episode')
-        self.fill_value = Tensor(params.get('init_size'), ms.int32)
-        self.episode_rewards = Parameter(Tensor([0.], ms.float32), requires_grad=False)
+        self.continuous_actions = params.get("continuous_actions")
+        self.num_agent = params.get("num_agent")
+        self.num_eval_episode = params.get("num_eval_episode")
+        self.fill_value = Tensor(params.get("init_size"), ms.int32)
+        self.episode_rewards = Parameter(Tensor([0.0], ms.float32), requires_grad=False)
         self.train_step = Parameter(Tensor(0, ms.int32), requires_grad=False)
 
     def train(self, episodes, callbacks=None, ckpt_path=None):
@@ -68,7 +70,6 @@ class MADDPGTrainer(Trainer):
         self.init()
         # 1 Using `CallbackManager` to traverse each callback.
         with CallbackManager(callbacks) as callback_list:
-
             # 2 Init or restore the variables if the checkpoint files exist.
             cb_params.cur_episode = 0
             if self.vars:
@@ -77,7 +78,7 @@ class MADDPGTrainer(Trainer):
             callback_list.begin(cb_params)
 
             # 3 Get `evaluate` function if meet the conditions.
-            if 'eval_rate' in cb_params and cb_params.eval_rate > 0:
+            if "eval_rate" in cb_params and cb_params.eval_rate > 0:
                 cb_params.evaluate = self.evaluate
             episode_rewards = []
             start = time.time()
@@ -90,13 +91,22 @@ class MADDPGTrainer(Trainer):
                 elif len(ans) == 4:
                     loss, rewards, steps, others = ans
                 else:
-                    raise RuntimeError("The output number of function `train_one_episode` must be 3 or 4, \
-                        and represent for `loss, rewards, steps, [optional]others.` in order")
+                    raise RuntimeError(
+                        "The output number of function `train_one_episode` must be 3 or 4, \
+                        and represent for `loss, rewards, steps, [optional]others.` in order"
+                    )
                 episode_rewards.append(float(rewards.asnumpy()))
                 if i % 1000 == 0:
                     print("-----------------------------------------")
-                    print("In episode {}, mean episode reward is {} , cost {} s.".format(\
-                        i, np.mean(episode_rewards[-1000:]), round(time.time()-start, 3)))
+                    # pylint: disable=C0209
+                    print(
+                        "In episode {}, mean episode reward is {} , cost {} s.".format(
+                            i,
+                            np.mean(episode_rewards[-1000:]),
+                            round(time.time() - start, 3),
+                        ),
+                        flush=True,
+                    )
                     start = time.time()
                 cb_params.loss = loss
                 cb_params.total_rewards = rewards
@@ -118,14 +128,21 @@ class MADDPGTrainer(Trainer):
             act_n = []
             agent_id = 0
             while agent_id < self.num_agent:
-                act_n.append(self.agent[agent_id].actor.sample_action(self.continuous_actions,\
-                     obs_n[agent_id, :].squeeze()))
+                act_n.append(
+                    self.agent[agent_id]
+                    .actor.sample_action(
+                        self.continuous_actions, obs_n[agent_id, :].expand_dims(0)
+                    )
+                    .squeeze()
+                )
                 agent_id += 1
             action_list = self.stack_first(act_n)
             # step for each agent's env
             new_obs_n, rew_n, done_n = self.msrl.collect_environment.step(action_list)
 
-            self.msrl.replay_buffer_insert([obs_n, action_list, rew_n, new_obs_n, done_n])
+            self.msrl.replay_buffer_insert(
+                [obs_n, action_list, rew_n, new_obs_n, done_n]
+            )
             obs_n = new_obs_n
             done = done_n.all()
             if done:
@@ -148,8 +165,13 @@ class MADDPGTrainer(Trainer):
             act_n = []
             agent_id = 0
             while agent_id < self.num_agent:
-                act_n.append(self.agent[agent_id].actor.sample_action(self.continuous_actions,\
-                     obs_n[agent_id, :].squeeze()))
+                act_n.append(
+                    self.agent[agent_id]
+                    .actor.sample_action(
+                        self.continuous_actions, obs_n[agent_id, :].expand_dims(0)
+                    )
+                    .squeeze()
+                )
                 agent_id += 1
             action_list = self.stack_first(act_n)
             # step for each agent's env
@@ -157,7 +179,9 @@ class MADDPGTrainer(Trainer):
 
             dones = done_n.all()
 
-            self.msrl.replay_buffer_insert([obs_n, action_list, rew_n, new_obs_n, done_n])
+            self.msrl.replay_buffer_insert(
+                [obs_n, action_list, rew_n, new_obs_n, done_n]
+            )
 
             obs_n = new_obs_n
             duration += 1
@@ -165,12 +189,23 @@ class MADDPGTrainer(Trainer):
 
             # ----------------------------------------- learner -------------------------------------------
             if self.train_step % 100 == 0:
-                obs_n_batch, act_n_batch, rew_n_batch, obs_next_n_batch, done_n_batch \
-                    = self.msrl.replay_buffer_sample()
+                (
+                    obs_n_batch,
+                    act_n_batch,
+                    rew_n_batch,
+                    obs_next_n_batch,
+                    done_n_batch,
+                ) = self.msrl.replay_buffer_sample()
                 agent_id = 0
                 while agent_id < self.num_agent:
-                    loss += self._learn(agent_id, obs_n_batch, act_n_batch, rew_n_batch, \
-                        obs_next_n_batch, done_n_batch)
+                    loss += self._learn(
+                        agent_id,
+                        obs_n_batch,
+                        act_n_batch,
+                        rew_n_batch,
+                        obs_next_n_batch,
+                        done_n_batch,
+                    )
                     agent_id += 1
 
             if dones:
@@ -194,7 +229,11 @@ class MADDPGTrainer(Trainer):
                 act_n = []
                 agent_id = 0
                 while agent_id < self.num_agent:
-                    act_n.append(self.agent[agent_id].actor.get_action(self.continuous_actions, obs_n[agent_id, :]))
+                    act_n.append(
+                        self.agent[agent_id].actor.get_action(
+                            self.continuous_actions, obs_n[agent_id, :]
+                        )
+                    )
                     agent_id += 1
                 action_list = self.stack_first(act_n)
                 obs_n, rew_n, done_n = self.msrl.eval_environment.step(action_list)
@@ -207,15 +246,20 @@ class MADDPGTrainer(Trainer):
         return eval_episode_rewards / self.num_eval_episode
 
     def _learn(self, agent_id, obs_n, act_n, rew_n, obs_next_n, done_n):
-        '''learn func for each agent'''
+        """learn func for each agent"""
         rew, done = rew_n[:, agent_id, :], done_n[:, agent_id, :]
         # comput target_act_next_n
         target_act_next_n = []
         i = 0
         while i < self.num_agent:
-            target_act_next_n.append(self.agent[i].actor.sample_action(self.continuous_actions,\
-                 obs_next_n[:, i, :], use_target=True))
+            target_act_next_n.append(
+                self.agent[i].actor.sample_action(
+                    self.continuous_actions, obs_next_n[:, i, :], use_target=True
+                )
+            )
             i += 1
         target_act_next = self.cat(target_act_next_n)
-        loss = self.agent[agent_id].learner.learn([obs_n, act_n, obs_next_n, rew, done, target_act_next, agent_id])
+        loss = self.agent[agent_id].learner.learn(
+            [obs_n, act_n, obs_next_n, rew, done, target_act_next, agent_id]
+        )
         return loss
